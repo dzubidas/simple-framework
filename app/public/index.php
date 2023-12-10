e5d455fc3bef7030aaf658f84366a4b0a050f2a4
Michal <majky@drupalarts.cz>
2023-12-10 22:07:17 +0100

    index.php

--- app/public/index.php
+++ app/public/index.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 use dockerphp\framework\Http\Request;
 use dockerphp\framework\Http\Response;
+use dockerphp\framework\Http\HttpKernel;
+
+define('BASE_PATH', dirname(__DIR__));
+define('ROOT_PATH', dirname(__FILE__). DIRECTORY_SEPARATOR);
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $request = Request::createFromGlobals();
+$kernel = new HttpKernel();
 
-$content = '<h1>Hello World!</h1>';
-$response = new Response(content: $content, status: 200, headers: []);
+$response = $kernel->handle($request);
 $response->send();
-
-// dd($request);
