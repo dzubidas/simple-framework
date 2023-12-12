d887d09d97246e69cc18eb0e3ee4171707e05200
Michal <majky@drupalarts.cz>
2023-12-12 21:24:30 +0100

    database connection

--- app/public/index.php
+++ app/public/index.php
@@ -9,7 +9,26 @@ use dockerphp\framework\Http\HttpKernel;
 define('BASE_PATH', dirname(__DIR__));
 define('ROOT_PATH', dirname(__FILE__). DIRECTORY_SEPARATOR);
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php'; 
+
+try {
+
+    // $dsn = "mysql:host=db;dbname=app_test";
+    $dsn = "mysql:host=$_ENV[MYSQL_HOST];dbname=$_ENV[MYSQL_DATABASE]";
+    $db = new \PDO($dsn, $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD'] , []);    
+
+    $query = 'SELECT * FROM users';
+    $stmt = $db->query($query);
+    $users = $stmt->fetchAll(PDO::FETCH_OBJ);
+    
+    foreach ($users as $user) {
+        echo $user->name . '<br>';
+    }
+
+
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), $e->getCode());
+}
 
 $request = Request::createFromGlobals();
 $kernel = new HttpKernel();
